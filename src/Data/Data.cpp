595de49ec14bb1d7c7f521522d1d59bb57326176
Geordani Machado <74798854+Geordani-Machado@users.noreply.github.com>
2022-04-21 01:33:00 +0000

    implement Data

--- src/Data/Data.cpp
+++ src/Data/Data.cpp
@@ -1,1 +1,33 @@
 #include "Data.hpp"
+
+void Data::set_dia(int d){
+	dia = d;
+}
+
+void Data::set_mes(int m){
+  mes = m;
+}
+
+void Data::set_ano(int a){
+  ano = a;
+}
+
+void Data::formatar(){
+  dataFormatada = to_string(dia) + "/" + to_string(mes) + "/" + to_string(ano);
+}
+int Data::get_dia(){
+	return dia;
+}
+
+string Data::get_dataFormatada(){
+  formatar();
+	return dataFormatada;
+}
+
+int Data::get_mes(){
+  return mes;
+}
+
+int Data::get_ano(){
+  return ano;
+}
